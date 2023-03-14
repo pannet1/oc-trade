2ae66fc97afb651e1b9e2b106904e7b5c2555577
karthick <prog@ecomsense.in>
2023-03-15 02:32:14 +0530

    copy before merge

--- run_algo.bat
+++ run_algo.bat
@@ -1,2 +1,2 @@
 @echo on
-cmd /k "cd /d E:\py\py39\Scripts & .\activate & cd /d E:\py\py39\oc-trade\oc_trade & py main.py"
+cmd /k "cd /d E:\PY\py39\Scripts & .\activate & cd /d E:\PY\py39\oc-trade\oc_trade & py main.py"
